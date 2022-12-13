ecdb6be23a08599179056a4a4e283c127a83f886
arun <cvaruncv1998@gmail.com>
2022-12-13 11:44:45 +0530

    cleanup in app

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import ProtectedRoutes from './routes.js/protectedRoutes';
@@ -11,9 +11,11 @@ import store from './redux/store';
 
 
 
-
 function App() {
-
+  
+  useEffect(()=>{
+  return ()=>localStorage.clear()
+  },[])
   return (
     <GoogleOAuthProvider clientId={`${process.env.REACT_APP_google_client_id}`} >
       <Provider store={store}>
