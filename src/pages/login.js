9d7556c3dd752be10aa2e6e09a513b430cf18e6f
arun <cvaruncv1998@gmail.com>
2024-03-25 14:30:03 +0530

    small changes for login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -58,14 +58,14 @@ function Login() {
         <div >
 
             <section className="bg-gray-50 min-h-screen flex flex-col justify-center ">
-                <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+                <div className="flex flex-col items-center justify-center md:px-6 py-8 md:mx-auto mx-4 md:h-screen lg:py-0">
                     {/* <div className="flex items-center mb-6 text-2xl font-semibold text-gray-900 ">
                         <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo" />
                         Flowbite
                     </div> */}
                     <div className="w-full bg-white rounded-lg shadow  md:mt-0 sm:max-w-md xl:p-0 ">
                         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-                            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
+                            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl text-center ">
                                 Sign in to your account
                             </h1>
                             <div className=' w-full flex justify-center'>
